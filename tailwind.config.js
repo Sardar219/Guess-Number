aad2cf0421a88bddcfd71895fd7ab58be3b59555
Sardar219 <sardarhossaini2022@gmail.com>
2024-07-18 19:04:25 +0430

    setup the tailwindcss for guess the number project

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./dist/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        secondary: "#888883",
+      },
+    },
+  },
+  plugins: [],
+};
