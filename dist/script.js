33c98c8b8fba682f4cdfca2be14a3914765fa432
Sardar219 <sardarhossaini2022@gmail.com>
2024-07-18 20:58:43 +0430

    dubage the js file

--- dist/script.js
+++ dist/script.js
@@ -10,38 +10,12 @@ const body = document.querySelector("#body");
 let counter = 20;
 let total = 0;
 let computerguess = Math.trunc(Math.random() * 20) + 1;
-
+console.log(computerguess);
 ///////// check part  /////////////
-function changeColor() {
-  guaidtext.textContent = "🎉 correct number !";
-  body.classList.remove("bg-gray-800");
-  body.classList.add("bg-green-500");
-}
-function changeColorf() {
-  guaidtext.textContent = "💥 You lost the game!";
-  body.classList.remove("bg-gray-800");
-  body.classList.add("bg-red-800");
-}
-function changeBack() {
-  if (body.classList.contains("bg-green-500")) {
-    body.classList.remove("bg-green-500");
-    body.classList.add("bg-gray-800");
-  } else {
-    body.classList.remove("bg-red-800");
-    body.classList.add("bg-gray-800");
-  }
-}
-againbtn.addEventListener("click", function () {
-  computerguess = Math.trunc(Math.random() * 20) + 1;
-  changeBack();
-  counter = 20;
-  guaidtext.textContent = "Start guessing ...";
-  userguess.value = "";
-  shownumber.textContent = "?";
-  score.textContent = 20;
-});
 checkbtn.addEventListener("click", function () {
   const uservalue = Number(userguess.value);
+  userguess.value = "";
+  console.log(userguess.value);
   // /////// if user dont't enter anything ///////
   if (!uservalue) {
     guaidtext.textContent = "⛔ No number !";
@@ -75,4 +49,33 @@ checkbtn.addEventListener("click", function () {
       changeColorf();
     }
   }
+});
+
+function changeColor() {
+  guaidtext.textContent = "🎉 correct number !";
+  body.classList.remove("bg-gray-800");
+  body.classList.add("bg-green-500");
+}
+function changeColorf() {
+  guaidtext.textContent = "💥 You lost the game!";
+  body.classList.remove("bg-gray-800");
+  body.classList.add("bg-red-800");
+}
+function changeBack() {
+  if (body.classList.contains("bg-green-500")) {
+    body.classList.remove("bg-green-500");
+    body.classList.add("bg-gray-800");
+  } else {
+    body.classList.remove("bg-red-800");
+    body.classList.add("bg-gray-800");
+  }
+}
+againbtn.addEventListener("click", function () {
+  computerguess = Math.trunc(Math.random() * 20) + 1;
+  changeBack();
+  counter = 20;
+  guaidtext.textContent = "Start guessing ...";
+  userguess.value = "";
+  shownumber.textContent = "?";
+  score.textContent = 20;
 });
